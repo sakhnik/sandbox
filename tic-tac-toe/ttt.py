05ca569eedf63aa2a4e0d6228d57b72b09de13e3
Anatolii Sakhnik <sakhnik@gmail.com>
2024-04-14 20:28:41 +0300

    Try naive learning

--- tic-tac-toe/ttt.py
+++ tic-tac-toe/ttt.py
@@ -43,57 +43,98 @@ def prune(strategy, history):
     for i in range(-2, -len(history), -2):
         position, move = history[i]
         possible_moves = strategy.get(position)
-        possible_moves.remove(move)
+        if move in possible_moves:
+            possible_moves.remove(move)
         if not possible_moves:
             break
 
 
-def main():
+def test_match(strategy):
     board = [[" " for _ in range(3)] for _ in range(3)]
     current_player = "X"
     history = []
-    strategy = {}
-
-    print("Welcome to Tic Tac Toe!")
 
     while True:
-        print_board(board)
-        key = input(f"Your move {current_player} (1-9): ")
-        if key == '.':
-            cur_board = get_board_str(board)
-            possible_moves = strategy.get(cur_board)
-            if not possible_moves:
-                possible_moves = {(row, col)
-                                  for row in range(3) for col in range(3)
-                                  if board[row][col] == ' '}
-                strategy[cur_board] = possible_moves
-            row, col = random.choice(list(possible_moves))
-        else:
-            move = int(key)
-            row = 2 - (move - 1) // 3
-            col = (move - 1) % 3
-
-        if board[row][col] != " ":
-            print("Cell already occupied. Try again.")
-            continue
+        cur_board = get_board_str(board)
+        possible_moves = strategy.get(cur_board)
+        if not possible_moves:
+            possible_moves = {(row, col)
+                              for row in range(3) for col in range(3)
+                              if board[row][col] == ' '}
+            strategy[cur_board] = possible_moves
+        row, col = random.choice(list(possible_moves))
 
         history.append((get_board_str(board), (row, col)))
         board[row][col] = current_player
 
         if check_win(board, current_player):
-            print_board(board)
-            print(f"Player {current_player} wins!")
             break
 
         if is_board_full(board):
-            print_board(board)
-            print("It's a tie!")
             break
 
         current_player = "O" if current_player == "X" else "X"
 
     if check_win(board, current_player):
         prune(strategy, history)
+
+
+def main():
+    strategy = {}
+
+    for i in range(100000):
+        test_match(strategy)
+
+    while True:
+        board = [[" " for _ in range(3)] for _ in range(3)]
+        current_player = "X"
+        history = []
+
+        print()
+        print("Welcome to Tic Tac Toe!")
+
+        while True:
+            print_board(board)
+            key = input(f"Your move {current_player} (1-9): ")
+            if key == '.' or key == ',':
+                cur_board = get_board_str(board)
+                possible_moves = strategy.get(cur_board)
+                if not possible_moves:
+                    possible_moves = {(row, col)
+                                      for row in range(3) for col in range(3)
+                                      if board[row][col] == ' '}
+                    strategy[cur_board] = possible_moves
+                row, col = random.choice(list(possible_moves))
+            else:
+                try:
+                    move = int(key)
+                    row = 2 - (move - 1) // 3
+                    col = (move - 1) % 3
+                except ValueError:
+                    print("Incorrect move")
+                    continue
+
+            if board[row][col] != " ":
+                print("Cell already occupied. Try again.")
+                continue
+
+            history.append((get_board_str(board), (row, col)))
+            board[row][col] = current_player
+
+            if check_win(board, current_player):
+                print_board(board)
+                print(f"Player {current_player} wins!")
+                break
+
+            if is_board_full(board):
+                print_board(board)
+                print("It's a tie!")
+                break
+
+            current_player = "O" if current_player == "X" else "X"
+
+        if check_win(board, current_player):
+            prune(strategy, history)
 
 
 if __name__ == "__main__":
