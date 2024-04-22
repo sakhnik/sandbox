4767addf4d5daa547c8f791966e61f5f3c9da50b
Anatolii Sakhnik <sakhnik@gmail.com>
2024-04-22 20:54:49 +0300

    Count bulls by common digits

--- 02-bulls-cows/bc.lua
+++ 02-bulls-cows/bc.lua
@@ -3,13 +3,27 @@ math.randomseed(os.time())
 print "Guess a secret number"
 
 local number = math.random(1000, 9999)
-local digits = {}
 
-digits[number % 10] = 1
-digits[math.floor(number / 10) % 10] = 1
-digits[math.floor(number / 100) % 10] = 1
-digits[math.floor(number / 1000)] = 1
---vim.print(digits)
+local function count_digits(a)
+    local digits = {}
+    for i = 0, 9 do
+        digits[i] = 0
+    end
+
+    local function add_digit(d)
+        digits[d] = digits[d] + 1
+    end
+
+    add_digit(a % 10)
+    add_digit(math.floor(a / 10) % 10)
+    add_digit(math.floor(a / 100) % 10)
+    add_digit(math.floor(a / 1000))
+    return digits
+end
+
+local digits = count_digits(number)
+
+-- vim.print(digits)
 
 while true do
     io.write "Your guess: "
@@ -19,11 +33,17 @@ while true do
         break
     end
 
+    local guess_digits = count_digits(guess)
     local bulls = 0
-    if digits[guess % 10] == 1                      then bulls = bulls + 1 end
-    if digits[math.floor(guess / 10) % 10] == 1     then bulls = bulls + 1 end
-    if digits[math.floor(guess / 100) % 10] == 1    then bulls = bulls + 1 end
-    if digits[math.floor(guess / 1000)] == 1        then bulls = bulls + 1 end
+    for i = 0, 9 do
+        if guess_digits[i] > 0 then
+            if digits[i] < guess_digits[i] then
+                bulls = bulls + digits[i]
+            else
+                bulls = bulls + guess_digits[i]
+            end
+        end
+    end
 
     local cows = 0
     if number % 10 == guess % 10 then cows = cows + 1 end
